6c3e3f588502a4127a5eee7860551c7dbfe559c0
Benjamin Suter <BenjaminSuter@student-net-als-0178.ethz.ch>
2020-10-06 12:14:00 +0200

    Filtered Stream using API v2

---
+++ FilteredStreamAPI2.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 11:17:01 2020
+
+@author: BenjaminSuter
+"""
+import requests
+import os
+import json
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def get_rules(headers, bearer_token):
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", headers=headers
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+    return response.json()
+
+
+def delete_all_rules(headers, bearer_token, rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        headers=headers,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print(json.dumps(response.json()))
+
+
+def set_rules(headers, delete, bearer_token):
+    '''
+    We can pass 'sample_rules' as an argument where we read the rules from a
+    a file beforhand. 
+    '''
+    # You can adjust the rules if needed
+    sample_rules = [
+        {"value": "dog has:images", "tag": "dog pictures"},
+        {"value": "cat has:images -grumpy", "tag": "cat pictures"},
+    ]
+    payload = {"add": sample_rules}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        headers=headers,
+        json=payload,
+    )
+    if response.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+
+
+def get_stream(headers, set, bearer_token):
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream", headers=headers, stream=True,
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+def main():
+    '''
+    ToDo:
+        Catch connection errors and time outs using the errors and exceptions 
+        from requests
+        https://requests.readthedocs.io/en/latest/user/quickstart/
+        
+        When appropriat, try to reconnect.
+
+    Returns
+    -------
+    None.
+
+    '''
+    bearer_token = os.environ.get("BEARER_TOKEN")
+    headers = create_headers(bearer_token)
+    rules = get_rules(headers, bearer_token)
+    delete = delete_all_rules(headers, bearer_token, rules)
+    set = set_rules(headers, delete, bearer_token)
+    get_stream(headers, set, bearer_token)
+
+
+if __name__ == "__main__":
+    main()
